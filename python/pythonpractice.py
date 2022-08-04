624dd39165657adde2e44bc348bbff870a9d9d48
Fiona Eckert <fionameckert@gmail.com>
2022-08-04 17:24:43 -0500

    adding 08 04 hw

--- python/pythonpractice.py
+++ python/pythonpractice.py
@@ -1,4 +1,5 @@
 # Day 4
+from termios import FIONREAD
 from tkinter.messagebox import QUESTION
 
 
@@ -70,3 +71,75 @@ print(numbers[-2])
 
 print(hex(id(numbers)))
 print(hex(id(numbers[1])))
+
+# Lexicon comparison - essentially a string comparison
+mysteryLetter = 'B'
+firstNameLetter = 'b'
+if firstNameLetter < mysteryLetter:
+    print('firstname comes before mystery letter')
+else:
+    print('mystery letters comes before first name')
+
+students = ['Matt','Carlos','An','Fiona']
+breakoutroom1 = []
+breakoutroom2 = []
+cutoffLetter = 'I'
+for element in students:
+    if element[0]<=cutoffLetter:
+        breakoutroom1.append(element)
+    elif element[0]>cutoffLetter:
+        breakoutroom2.append(element)
+
+print(breakoutroom1)
+print(breakoutroom2)
+
+
+# Python programming language key works
+# if, for, or, and, while, class, return, pass
+
+# String concatenation (string manipulation)
+firstName = 'Fiona'
+lastName = 'Eckert'
+fullName = firstName+' '+lastName
+print(fullName)
+#strings can only be added together, you can't subtract
+#cannot combine a string with an integer or float but you could do the following:
+diceRoll = 9
+fullName = firstName + ' ' + lastName + str(diceRoll)
+#or you could put 9 in quotes ('9')
+
+
+#Write a short program that prints each number from 1 to 100 on a new line. 
+
+#For each multiple of 3, print "Fizz" instead of the number. 
+
+#For each multiple of 5, print "Buzz" instead of the number. 
+
+#For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+
+
+for i in range(1,101):
+    if i%3==0 and i%5!=0:
+        print('Fizz')
+    elif i%5==0 and i%3!=0:
+        print('Buzz')
+    elif i%3==0 and i%5==0:
+        print('FizzBuzz')
+    else:
+        print(i)
+
+# how to reverse string and list
+sample = 'Hello world'
+print(sample[::-1])
+
+sampleA=['third','second','first']
+#print(sampleA[::-1])
+sampleB=[]
+
+i = len(sampleA)-1
+while i >=0:
+    sampleB.append(sampleA[i]) 
+    i = i-1
+print(sampleB)
+
+
---
+++ python/pythonpractice2.py
@@ -0,0 +1,16 @@
+sampleA=['third','second','first']
+sampleB=[]
+
+i = len(sampleA)-1
+while i >=0:
+    sampleB.append(sampleA[i]) 
+    i = i-1
+print(sampleB)
+
+# How to print a reversed list
+ordinalList = ['1st', '2nd', '3rd']
+reversedOrdinalList = []
+while len(ordinalList) > 0:
+    ordinal = ordinalList.pop()
+    reversedOrdinalList.append(ordinal)
+print(reversedOrdinalList)
